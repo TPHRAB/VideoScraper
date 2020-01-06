fb2c7438d9fdaa90f267eaf17b6a908d824e65d2
TPHRAB <chenze.zhao0@outlook.com>
2020-01-05 18:31:04 -0800

    BruteForceHTML module

--- src/modules/BruteforceHTML/BruteForceHTML.java
+++ src/modules/BruteforceHTML/BruteForceHTML.java
@@ -7,11 +7,14 @@ import javafx.scene.input.MouseEvent;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BruteForceHTML {
+    private String[] formats = {"MP4", "VOB", "M4S", "m3u8"};
 
     @FXML
     private TextField urlTextField;
@@ -32,19 +35,32 @@ public class BruteForceHTML {
 
     @FXML
     void startButtonClicked(MouseEvent event) {
+        getFormatRegex();
         String url = urlTextField.getText().trim();
         String downloadPath = downloadPathTextField.getText().trim();
 
 
+        System.setProperty("webdriver.gecko.driver", "./geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get(url);
-        Matcher matcher = Pattern.compile("\"https?://[^\"]*?mp4.*?\"").matcher(driver.getPageSource());
+        Matcher matcher = Pattern.compile(getFormatRegex()).matcher(driver.getPageSource());
+        Set<String> videoURLs = new HashSet<>(); // store all unique video urls
         while (matcher.find()) {
-            System.out.println(matcher.group());
+            videoURLs.add(matcher.group());
         }
         // close browser
         driver.close();
+
+    }
+
+    private String getFormatRegex() {
+        String regex = "(";
+        for (int i = 0; i < formats.length - 1; i++) {
+            regex += formats[i] + "|";
+        }
+        regex += formats[formats.length - 1] + ")";
+        return "\"https?://[^\"=]*?" + regex + ".*?\"";
     }
 
 }
